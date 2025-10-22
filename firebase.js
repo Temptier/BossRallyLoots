2e052afd2bb57fa7d936798fb22f671a38fe4019
Temptier <mmmcoc3@gmail.com>
2025-10-22 13:59:41 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,30 +1,19 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
-import { getFirestore, collection, doc, getDoc, setDoc, updateDoc, arrayUnion } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
+import { getFirestore, collection, doc, getDoc, setDoc, updateDoc, arrayUnion, deleteDoc } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
 
+// Your Firebase config
 const firebaseConfig = {
-
-
-  apiKey: "AIzaSyCi4ldLdVtWAUKb0wyyds2HnbNujjIHmWQ",
-
-
-  authDomain: "guildrallyloots.firebaseapp.com",
-
-
-  projectId: "guildrallyloots",
-
-
-  storageBucket: "guildrallyloots.firebasestorage.app",
-
-
-  messagingSenderId: "116266984921",
-
-
-  appId: "1:116266984921:web:90326a9fed2ee48f79a5c8"
-
-
+  apiKey: "AIzaSyCi4ldLdVtWAUKb0wyyds2HnbNujjIHmWQ",
+  authDomain: "guildrallyloots.firebaseapp.com",
+  projectId: "guildrallyloots",
+  storageBucket: "guildrallyloots.firebasestorage.app",
+  messagingSenderId: "116266984921",
+  appId: "1:116266984921:web:90326a9fed2ee48f79a5c8"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, doc, getDoc, setDoc, updateDoc, arrayUnion };
+export { db, collection, doc, getDoc, setDoc, updateDoc, arrayUnion, deleteDoc };
